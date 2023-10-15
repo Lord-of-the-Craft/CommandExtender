349c1a6c5a4968918a2e10ba9664a775a9d8b180
Adelemphii <demilitioowner@gmail.com>
2023-10-14 19:07:06 -0500

    init commit

---
+++ src/main/java/net/lordofthecraft/commandextender/CommandExtender.java
@@ -0,0 +1,17 @@
+package net.lordofthecraft.commandextender;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CommandExtender extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
